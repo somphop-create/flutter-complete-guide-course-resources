3be8dd0bab4d525a228d59d20f81454e3ff1ffbb
SS <161840369+somphop-create@users.noreply.github.com>
2024-12-24 10:22:27 +0000

    Update gradient_container.dart

--- Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/gradient_container.dart
+++ Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/gradient_container.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:basics/dice_roller.dart';
 
-//declare variable here for convenience
+//4 declare variable here for convenience
 //variables should be const not var because these values here should not be change thereafter
 //and the values are constant values which can be loged-in at the complie time
 const startAlignment = Alignment.topLeft; 
@@ -11,16 +11,20 @@ const endAlignment = Alignment.bottomRight;
 //but cannot be used here since 'begin' cannot accept null
 
 class GradientContainer extends StatelessWidget { //1 add logic from Statelesswidget to MyApp class
-  const GradientContainer(this.color1, this.color2, {super.key}); //Initialization
-
+  const GradientContainer(this.color1, this.color2, {super.key}); //Initialization with super.key
+  //5 to use constructor function, we have to accept parameters here (color1 and color2 here)
+  //becasue Dart will know which variables to look for in the class
+  //they should be positional parameter because it is required (named paprameter is optional)
+  
   const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
+  //6 the variables have to be declared and stored inside class because Dart won't know if we don't do?
 
-  @override //3 Make it clear we are
+  @override //3 make it clear we are using widget by overriding it
   Widget build(context) { //2 build method (function) is required for building custom widget
                           //Widget is a return value type of this build function
                           //Context parameter is needed here to address the widget position
