7252c43e9bda12f629632f47c4ca9e157a685910
SS <161840369+somphop-create@users.noreply.github.com>
2024-12-24 23:57:43 +0100

    Update dice_roller.dart

--- Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/dice_roller.dart
+++ Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/dice_roller.dart
@@ -26,13 +26,13 @@ class _DiceRollerState extends State<DiceRoller> {
   @override
   Widget build(context) {
     return Column( //add column widget to display content vertically
-      mainAxisSize: MainAxisSize.min,
+      mainAxisSize: MainAxisSize.min, //to minimize the size of column to litmit only within the screen
       children: [ //children is properties of column widget
         Image.asset( //Image is a built.in widget and has asset as a default constructor
           'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 20), //fixed size, cutting off contents if they are too long 
         TextButton(
           onPressed: rollDice, //rollDice function as a value, don't exacute but point to that function
           style: TextButton.styleFrom(
