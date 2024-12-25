5ce306ebcca4cb025f998db7a0ecdfd827df2b76
SS <161840369+somphop-create@users.noreply.github.com>
2024-12-25 12:17:59 +0100

    Update dice_roller.dart

--- Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/dice_roller.dart
+++ Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/dice_roller.dart
@@ -4,37 +4,41 @@ import 'package:flutter/material.dart';
 
 final randomizer = Random();
 
-class DiceRoller extends StatefulWidget {
-  const DiceRoller({super.key});
+class DiceRoller extends StatefulWidget { //3 create a class extending StatefulWidget
+  const DiceRoller({super.key}); //constructor function as always
 
   @override
-  State<DiceRoller> createState() {
-    return _DiceRollerState();
+  State<DiceRoller> createState() { //4 do not need build method, but createState method
+                                    //return type is 'State' prase DiceRoller state (our class)
+    return _DiceRollerState();      //return value that have State type (create another class below)
   }
 }
 
-class _DiceRollerState extends State<DiceRoller> {
+class _DiceRollerState extends State<DiceRoller> { //5 return value
+                                                   //typically starts with _ and appends 'State' to the end of parent widget above
+                                                   //_ means this class is only private in this file and uses internall
   var currentDiceRoll = 2;
 
-  void rollDice() { //function that will be called later within build method 
+  void rollDice() { //2 function that will be called later within build method 
                     //return void because onPressed wants void as a return type
-    setState(() {
+    setState(() { //7 fluter won't reexacute the build method below, and don't update the changes
+                 //we have to tell it to do it by setting state with needed function
       currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
-  @override
+  @override // 6 add build method (belongs to _DiceRollerState)
   Widget build(context) {
-    return Column( //add column widget to display content vertically
+    return Column( //1 add column widget to display content vertically
       mainAxisSize: MainAxisSize.min, //to minimize the size of column to litmit only within the screen
-      children: [ //children is properties of column widget
-        Image.asset( //Image is a built.in widget and has asset as a default constructor
+      children: [ //1 children is properties of column widget
+        Image.asset( //1 Image is a built.in widget and has asset as a default constructor
           'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
-        const SizedBox(height: 20), //fixed size, cutting off contents if they are too long 
+        const SizedBox(height: 20), //1 fixed size, cutting off contents if they are too long 
         TextButton(
-          onPressed: rollDice, //rollDice function as a value, don't exacute but point to that function
+          onPressed: rollDice, //2 rollDice function as a value, don't exacute but point to that function
           style: TextButton.styleFrom(
             // padding: const EdgeInsets.only(
             //   top: 20,
