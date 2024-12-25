36e77faa9edea8a0157fe625d1c409e47a3ac058
SS <161840369+somphop-create@users.noreply.github.com>
2024-12-25 14:16:42 +0100

    Update dice_roller.dart

--- Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/dice_roller.dart
+++ Code Snapshots/02 Flutter & Dart Basics I/15 Finished/lib/dice_roller.dart
@@ -2,7 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-final randomizer = Random();
+final randomizer = Random(); //11 avoid creating new object every time we roll the dice
+                             //by storing the Random() function into variable
 
 class DiceRoller extends StatefulWidget { //3 create a class extending StatefulWidget
   const DiceRoller({super.key}); //constructor function as always
@@ -17,13 +18,14 @@ class DiceRoller extends StatefulWidget { //3 create a class extending StatefulW
 class _DiceRollerState extends State<DiceRoller> { //5 return value
                                                    //typically starts with _ and appends 'State' to the end of parent widget above
                                                    //_ means this class is only private in this file and uses internall
-  var currentDiceRoll = 2;
+  var currentDiceRoll = 2; //8 store initial value
 
   void rollDice() { //2 function that will be called later within build method 
                     //return void because onPressed wants void as a return type
     setState(() { //7 fluter won't reexacute the build method below, and don't update the changes
                  //we have to tell it to do it by setting state with needed function
-      currentDiceRoll = randomizer.nextInt(6) + 1;
+      currentDiceRoll = randomizer.nextInt(6) + 1; // 9 random number using built-in function
+                                                   //nextInt generate a random integer between 0-(max)
     });
   }
 
@@ -33,7 +35,7 @@ class _DiceRollerState extends State<DiceRoller> { //5 return value
       mainAxisSize: MainAxisSize.min, //to minimize the size of column to litmit only within the screen
       children: [ //1 children is properties of column widget
         Image.asset( //1 Image is a built.in widget and has asset as a default constructor
-          'assets/images/dice-$currentDiceRoll.png',
+          'assets/images/dice-$currentDiceRoll.png', //10 append the generated number here using $
           width: 200,
         ),
         const SizedBox(height: 20), //1 fixed size, cutting off contents if they are too long 
